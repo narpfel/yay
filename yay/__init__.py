b78828a0f86039f3d7b5ffc0f5a0bc845e4c8554
Paul Gey <narpfel@gmx.de>
2017-05-28 23:46:35 +0200

    Add custom finder to `sys.meta_path`
    This allows importing yay files via the normal `import` statement.

--- yay/__init__.py
+++ yay/__init__.py
@@ -1,6 +1,9 @@
+import sys
+
 from yay.program import Program, sub, macro, block_macro, Mod
 from yay.helpers import (
     InvalidRegisterError, InvalidConfigError, WrongSignatureException,
+    YayFinder
 )
 
 
@@ -8,3 +11,6 @@ __all__ = [
     "Program", "sub", "macro", "block_macro", "Mod", "InvalidRegisterError",
     "InvalidConfigError", "WrongSignatureException",
 ]
+
+
+sys.meta_path.insert(0, YayFinder())
--- yay/helpers.py
+++ yay/helpers.py
@@ -1,9 +1,11 @@
 import ast
+import sys
 from collections.abc import Mapping
 from copy import deepcopy
 from functools import lru_cache, wraps
 from importlib.machinery import SourceFileLoader
 from importlib.util import module_from_spec, spec_from_loader
+from pathlib import Path
 from types import FunctionType, MethodType
 
 from pkg_resources import resource_filename
@@ -141,9 +143,30 @@ class YayFileLoader(SourceFileLoader):
         )
 
 
-def import_yay_file(yay_filename):
-    loader = YayFileLoader("main", yay_filename)
-    spec = spec_from_loader(loader.name, loader)
+def import_yay_file(yay_filename, name="main"):
+    spec = spec_for_yay_file(yay_filename, name)
     module = module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
+
+
+def spec_for_yay_file(yay_filename, module_name="main"):
+    loader = YayFileLoader(module_name, yay_filename)
+    return spec_from_loader(loader.name, loader)
+
+
+class YayFinder:
+    def find_on_path(self, name):
+        parts = name.split(".")
+        for path in map(Path, sys.path):
+            filename = path.joinpath(*parts).with_suffix(".yay")
+            if filename.exists():
+                return str(filename)
+
+    def find_spec(self, name, path, target=None):
+        if target is not None:
+            raise ImportError("`target is not None` case is not supported")
+
+        filename = self.find_on_path(name)
+        if filename is not None:
+            return spec_for_yay_file(filename, name)
